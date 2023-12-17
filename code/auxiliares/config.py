6e3451d0aa0c847230a86ccadb6ebe6782fab928
castrodiego <dcastro.contacto@gmail.com>
2023-12-17 20:39:03 +0000

    Se ajustan notebooks de estrategia intermedia 2 y se agrega un parametro para feature engineering (para hacerlo por categorias, prod, etc)

--- code/auxiliares/config.py
+++ code/auxiliares/config.py
@@ -22,24 +22,26 @@ ARCH_PRODUCTOS_PREDICCION_FUTURE="datasets/productos_a_predecir_201912.csv"
 ######## TRANSFORMACIONES #######
 #################################
 #TIPO_TRANSF_PARAM = "normalizacion"
-#TIPO_TRANSF_PARAM = "estandarizacion"
+TIPO_TRANSF_PARAM = "estandarizacion"
 
 #################################
 ###### FEATURE ENGINEERING ######
 #################################
-NUM_LAGS_PARAM = 5
+NUM_LAGS_PARAM = 15
 FAMILIA_FEATURES_TEMP_PARAM =["lags","delta_lags","rolling_mean","rolling_std","rolling_sum","bollinger_bands"]
+AMPLIA_FEATURES_PARAM=["cat1"]#,"cat2","cat3","customer_id","product_id"]
+
 
 ###########################
 ###### ENTRENAMIENTO ######
 ###########################
-CANT_ITERACIONES_OPT_BAY_PARAM = 10
+CANT_ITERACIONES_OPT_BAY_PARAM = 30
 
 MAX_BIN_PARAM=255
 #MAX_BIN_PARAM=1023
 
-OBJECTIVE_PARAM = "tweedie"
-#OBJECTIVE_PARAM = "regression"
+#OBJECTIVE_PARAM = "tweedie"
+OBJECTIVE_PARAM = "regression"
 #OBJECTIVE_PARAM = "regression_l1"
 
 
@@ -58,6 +60,6 @@ NUM_LEAVES_UPPER_PARAM=1024
 L1_UPPER_PARAM=1000.0
 L2_UPPER_PARAM=1000.0
 
-CANT_SEMILLAS=4
+CANT_SEMILLAS=10
 OFFSET_EVAL_HOLDOUT = 0
 CANT_EVAL_HOLDOUT = 5
